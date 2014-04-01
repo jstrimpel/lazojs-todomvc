e767435e2419f949c15792a8849650485e0413c7
Jason <JStrimpel@walmartlabs.com>
2014-04-01 10:28:00 -0700

    moved routes to external file

--- app/application.js
+++ app/application.js
@@ -7,17 +7,6 @@ define(['lazoApp'], function (LazoApp) {
         css: ['/app/client/base.css'],
 
         initialize: function (callback) {
-            LAZO.app.addRoutes({
-                '':             { component: 'todos-single' },
-                'multiple(/)':  { component: 'todos-multiple' },
-                'single(/)':    { component: 'todos-single' },
-                'layout(/)':    { component: 'main', layout: 'todos-layout' },
-                'header(/)':    { component: 'header' },
-                'main(/)':      { component: 'main' },
-                'footer(/)':    { component: 'footer' },
-                'hello(/)':    { component: 'hello', layout: 'todos-layout' },
-            });
-
             return callback();
         }
 
